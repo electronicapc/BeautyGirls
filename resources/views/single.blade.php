c88cbb7270214cfa9923d4957052c28b7a3668f3
c12789a <c12789a@LCOBGVITR8919.ena.us.experian.local>
2018-01-05 15:05:44 -0500

    Correcion telefono

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -67,8 +67,8 @@
 					  			<li class="list-group-item"><strong>Dos horas: </strong><p> ${{ $girls->v_two_h}}</p><a href="../pay/{{$girls->id}}/{{ $girls->v_two_h}}/2"><button class="btn btn-warning btn-sm">Lo quiero</button></a></li>
 					  			<li class="list-group-item"><strong>Tres horas: </strong><p> ${{ $girls->v_three_h}}</p><a href="../pay/{{$girls->id}}/{{ $girls->v_three_h}}/3"><button class="btn btn-warning btn-sm">Lo quiero</button></a></li>
 					  			<li class="list-group-item"><strong>Fin de Semana: </strong><p>${{ $girls->v_fds}}</p><a href="../pay/{{$girls->id}}/{{ $girls->v_fds}}/4"><button class="btn btn-warning btn-sm">Lo quiero</button></a></li>  -->
-					  			<h2>Contactame!!!!</h2>
-					  			<h2>Mi Tel&eacute;fono:</h2><strong>{{ $girls->email }}</strong>
+					  			<h3>Contactame!!!!</h3>
+					  			<h4>Mi Tel&eacute;fono:</h4><strong>{{ $girls->phone_number }}</strong>
 					  		</ul>	
 				      	
 						</div>
